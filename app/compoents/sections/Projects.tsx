0ac7af8e13c439eaf243aff00dfdfaf4fb221ebc
Benjamin <benjaminrowe0@gmail.com>
2026-08-04 18:21:39 +1000

    adding Simple Omelette to my projects

--- app/compoents/sections/Projects.tsx
+++ app/compoents/sections/Projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
         <h2 className="text-3xl md:text-4xl font-bold mb-8">
           Projects I Have Built
         </h2>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         <Card
             title="MVMNT Entertainment"
             description="A production website built and maintained from the ground up with CMS integration, monitoring, and ongoing support."
@@ -27,6 +27,13 @@ export default function Projects() {
             link="https://plumbing-landing-page-silk.vercel.app/"
             image="/project_images/image1.png"
             techstack={["Next.js", "Tailwind CSS", "Vercel"]}
+          />
+         <Card
+            title="Simple Omelette Recipe"
+            description="A responsive recipe page built with semantic HTML and modern CSS using a mobile-first approach, Flexbox, and media queries."
+            link="https://simple-omelette-recipe-topaz.vercel.app/"
+            image="/project_images/food_app.png"
+            techstack={["HTML5", "CSS3", "Flexbox"]}
           />
 
           
